a9e733e4e1543d911fbcde1f3309fb29eeaec1f7
Oswyn Brent <oswyn.brent@anchor.com.au>
2014-12-08 15:51:20 +1100

    update runner paths

--- run.sh
+++ run.sh
@@ -3,10 +3,9 @@
 set -eux
 
 yum update -y
-yum install -y python-requests m4 zlib-devel gmp-devel 
+yum install -y python-requests m4 zlib-devel gmp-devel
 
-git clone git@github.com:anchor/haskell2package ../haskell2package
-cd ../haskell2package
+cd haskell2package
 cabal update && cabal install --only-dependencies && cabal build
 cd ..
 exec ./haskell2package/dist/build/haskell2package/haskell2package $@
